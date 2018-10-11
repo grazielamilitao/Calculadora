de5b37c3130cd634d711a279ab3c90e515fc2a9b
04097711202 <ronaldpx20@gmail.com>
2018-10-11 16:18:47 -0400

    atualizaçao

--- JavaFXApplication1/src/Control/MenuController.java
+++ JavaFXApplication1/src/Control/MenuController.java
@@ -125,7 +125,7 @@ public class MenuController implements Initializable {
     private void Historico(ActionEvent event) throws IOException{
          
         FXMLLoader fxmlLoader = new FXMLLoader();
-        fxmlLoader.setLocation(getClass().getResource("/View.Histórico.fxml"));
+        fxmlLoader.setLocation(getClass().getResource("View/Histórico.fxml"));
         Scene scene = new Scene(fxmlLoader.load(),600,400);
         Stage stage = new Stage();
         stage.setTitle("Historico");
