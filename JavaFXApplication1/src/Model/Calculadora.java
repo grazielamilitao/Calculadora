4e5e3e51c50a350f1875d15ef8c8f87ffaf8834f
04097711202 <ronaldpx20@gmail.com>
2018-10-11 14:05:34 -0400

    atualização

--- JavaFXApplication1/src/Model/Calculadora.java
+++ JavaFXApplication1/src/Model/Calculadora.java
@@ -5,10 +5,65 @@
  */
 package Model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  *
  * @author 04097711202
  */
+@Entity
 public class Calculadora {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int txtCodigo;
+    
+    private double primeiroValor;
+    private double segundoValor;
+    private double operador;
+    private double resultado;
+
+    public int getTxtCodigo() {
+        return txtCodigo;
+    }
+
+    public void setTxtCodigo(int txtCodigo) {
+        this.txtCodigo = txtCodigo;
+    }
+
+    public double getPrimeiroValor() {
+        return primeiroValor;
+    }
+
+    public void setPrimeiroValor(double primeiroValor) {
+        this.primeiroValor = primeiroValor;
+    }
+
+    public double getSegundoValor() {
+        return segundoValor;
+    }
+
+    public void setSegundoValor(double segundoValor) {
+        this.segundoValor = segundoValor;
+    }
+
+    public double getOperador() {
+        return operador;
+    }
+
+    public void setOperador(double operador) {
+        this.operador = operador;
+    }
+
+    public double getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(double resultado) {
+        this.resultado = resultado;
+    }
+
 }
